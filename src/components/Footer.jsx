7fd3dcd9fd8faea1a8f2de138e2a954c8edfcafe
davidchuang5 <davidchuang5@gmail.com>
2024-02-19 13:50:58 -0600

    Created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+
+import { socialLinks } from '../constants';
+
+const Footer = () => {
+  return (
+    <footer clasName="footer font-poppins">
+      <hr className="border-slate-200" />
+
+      <div className="footer-container">
+        <p>
+          © 2024 <strong>David Chuang</strong>. All rights reserved.
+        </p>
+
+        <div className="flex gap-3 justify-center items-center">
+          {socialLinks.map((link) => (
+            <Link key={link.name} to={link.link} target="_blank">
+              <img src={link.iconUrl} alt={link.name} className="w-6 h-6 object-contain" />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
