4d2e5ecaa4ee58a767acdf9d238054f69ffe128b
davidchuang5 <davidchuang5@gmail.com>
2024-02-19 13:40:52 -0600

    Completed main menu messages and links

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import planeScene from '../assets/3d/bird.glb';
-import { useGLTF } from '@react-three/drei';
+import { useRef, useEffect } from 'react';
+import planeScene from '../assets/3d/plane.glb';
+import { useAnimations, useGLTF } from '@react-three/drei';
 
 const Plane = ({ isRotating, ...props }) => {
-  const { scene, animation } = useGLTF(planeScene);
+  const ref = useRef();
+  const { scene, animations } = useGLTF(planeScene);
+  const { actions } = useAnimations(animations, ref);
+
+  // useEffect(() => {
+  //   if (isRotating) {
+  //     actions['Take 001'].play();
+  //   } else {
+  //     actions['Take 001'].stop();
+  //   }
+  // }, [actions, isRotating]);
   return (
     <mesh {...props}>
       <primitive object={scene} position={[3, -1, 2]} scale={[0.003, 0.003, 0.003]} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ import Loader from '../components/Loader';
 import Island from '../models/Island';
 import Sky from '../models/Sky';
 import Plane from '../models/Plane';
-import Bird from '../models/Bird';
+import HomeInfo from '../components/HomeInfo';
+
 import { useState, useEffect } from 'react';
 {
   /* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
@@ -13,6 +14,7 @@ import { useState, useEffect } from 'react';
 }
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -29,9 +31,7 @@ const Home = () => {
   };
 
   const adjustPlaneForScreenSize = () => {
-    let screenScale = null;
-    let screenPosition = [0, -6.5, -43];
-    let rotation = [0.1, 4.7, 0];
+    let screenScale, screenPosition;
 
     if (window.innerWidth < 768) {
       screenScale = [1.5, 1.5, 1.5];
@@ -41,14 +41,17 @@ const Home = () => {
       screenPosition = [0, -4, -4];
     }
 
-    return [screenScale, screenPosition, rotation];
+    return [screenScale, screenPosition];
   };
 
-  const [islandScale, islandPosition, rotation] = adjustIslandForScreenSize();
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
   const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <HomeInfo currentStage={currentStage} />}
+      </div>
       <Canvas
         className={`'w-full h-screen bg-transparent' ${
           isRotating ? 'cursor-grabbing' : 'cursor-grab'
@@ -60,19 +63,20 @@ const Home = () => {
           <ambientLight intensity={0.5} />
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
           <Sky />
-          <Bird />
-          <Plane
-            position={planePosition}
-            scale={planeScale}
-            rotation={[0, 20, 0]}
-            isRotating={isRotating}
-          />
+
           <Island
             position={islandPosition}
             scale={islandScale}
-            rotation={rotation}
+            rotation={islandRotation}
             isRotating={isRotating}
             setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
+          />
+          <Plane
+            planePosition={planePosition}
+            planeScale={planeScale}
+            rotation={[0, 20, 0]}
+            isRotating={isRotating}
           />
         </Suspense>
       </Canvas>
