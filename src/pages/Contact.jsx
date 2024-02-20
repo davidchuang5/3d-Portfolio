8bf276b97ff283b06e3842103e1931349ff4e804
davidchuang5 <davidchuang5@gmail.com>
2024-02-20 12:38:24 -0600

    Initiated handlefunctions for form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import { useState, useRef } from 'react';
 
 const Contact = () => {
+  const [form, setForm] = useState({ name: '', email: '', message: '' });
+
+  const handleChange = (e) => {};
+
+  const handleFocus = (e) => {}; // To handle Fox movement
+
+  const handleBlur = (e) => {}; // To handle Fox movement
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
@@ -9,7 +16,16 @@ const Contact = () => {
         <form ref={formRef} onSubmit={handleSubmit} className="w-full flex flex-col gap-7 mt-14">
           <label className="text-black-500 font-semibold">
             Name
-            <input type="text" name="name" className="input" placeholder="Name" value={form.name} />
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Name"
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
           </label>
           <label className="text-black-500 font-semibold">
             Email
@@ -19,6 +35,9 @@ const Contact = () => {
               className="input"
               placeholder="validEmail@domain"
               value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
             />
           </label>
           <label className="text-black-500 font-semibold">
@@ -29,6 +48,9 @@ const Contact = () => {
               className="textarea"
               placeholder="Write your message here..."
               value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
             />
           </label>
           <button type="submit" className="btn">
