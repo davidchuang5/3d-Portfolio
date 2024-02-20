b36b3d7ffc8152ed8049c48dcfacdf25771f4af4
davidchuang5 <davidchuang5@gmail.com>
2024-02-20 14:48:59 -0600

    Email notification now working

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -18,7 +18,27 @@ const Contact = () => {
     e.preventDefault();
     setIsLoading(true);
 
-    emailjs.sendForm('service_xelm5w2', 'template_o48ybhm');
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: 'David',
+          from_email: form.email,
+          to_email: 'davidchuang5@gmail.com',
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+        setForm({ name: '', email: '', message: '' });
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.error(error);
+      });
   };
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
