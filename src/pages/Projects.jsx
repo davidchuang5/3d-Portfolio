5bcb2032d81d24f0265b36d8834001f81eca6a82
davidchuang5 <davidchuang5@gmail.com>
2024-02-21 11:49:30 -0600

    Created introduction to projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        My <span className="blue-gradient_text font-semibold drop-shadow"> Projects</span> 👋
+      </h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          I've created many projects over the years. Here are some of my favorites. If you see
+          something you like, feel free to check out the codebase and use it as inspiration for your
+          own projects! Your collaboration is highly valued.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
