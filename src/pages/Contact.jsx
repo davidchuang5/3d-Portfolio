a10899a2fd2a80e6a8a0bc7be9aeb009dc2291d7
davidchuang5 <davidchuang5@gmail.com>
2024-02-22 14:25:55 -0600

    Added emoji to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -77,7 +77,7 @@ const Contact = () => {
       {alert.show && <Alert {...alert} />}
 
       <div className="flex-1 min-w-[50%] flex flex-col">
-        <h1 className="head-text">Get in Touch</h1>
+        <h1 className="head-text">Get in Touch ☎️</h1>
 
         <form ref={formRef} onSubmit={handleSubmit} className="w-full flex flex-col gap-7 mt-14">
           <label className="text-black-500 font-semibold">
